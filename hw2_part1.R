1582ff0f78562eda53d87f69a4beeee64e028e58
Weidi Pan <weidipan@hotmail.com>
2018-10-22 23:22:26 -0500

    created a new function compare_outcomes
    created a new function to compare outcomes for random start values and different number of iterations

--- hw2_part1.R
+++ hw2_part1.R
@@ -48,7 +48,30 @@ summary(lm(y~x))
 #compare this procedure MCMC with linear regression
 
 
+#compare_outcomes, that takes as input an iteration number
+#for instance 1000, 10000, or 100000. Your function should loop 10 times; each 
+#time, it should initialize the MCMC chain with randomly selected start values 
+#for a, b, and sd and then run to completion after the required number of 
+#iterations. After each loop, the function should compute the mean and std 
+#of the values in the chain for a, and it should print these out. Test your 
+#function for 1,000, 10,000, and 100,000 iterations.
 
+
+compare_outcomes <- function(iterations) {
+  for (i in 1:10) {
+    a = runif(1, min=0, max=10)
+    b = rnorm(1, 0, 5)
+    sd = runif(1, min=0, max=30)
+    startvalue=c(a,b,sd)
+    chain = run_metropolis_MCMC(startvalue,iterations)
+    print(c(mean(chain[,1]),sd(chain[,1])))
+  }
+}
+
+
+compare_outcomes(1000)
+compare_outcomes(10000)
+compare_outcomes(100000)
 
 
 
